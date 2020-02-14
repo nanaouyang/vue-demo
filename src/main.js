48f8775fe9b7438ddb845ff5b286ce73f54e2472
chenc <chenc@fengyuntec.com>
2020-02-14 23:30:56 +0800

    添加饿了么ui

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./plugins/element.js";
 
 Vue.config.productionTip = false;
-
+Vue.prototype.$http = "aaa";
 new Vue({
   router,
   store,
