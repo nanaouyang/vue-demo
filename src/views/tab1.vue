030e1c7ce8e8683176e85ee50bedda1da3db12c9
chenc <chenc@fengyuntec.com>
2020-06-05 14:26:56 +0800

    ✨ 更新vue3

--- src/views/tab1.vue
+++ src/views/tab1.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
+    {{ test }}
     <ul>
       <li v-for="(item, index) in state.todoList" :key="index">{{ item }}</li>
     </ul>
     <button @click="increment">
       Count is: {{ state.count }}, double is: {{ state.double }}
     </button>
+    <button @click="test = '222'">修改</button>
+    <button @click="console">console</button>
   </div>
 </template>
 
 <script>
-import { reactive, computed } from "@vue/composition-api";
+import { reactive, computed, ref } from "@vue/composition-api";
 export default {
   name: "tab1",
   setup() {
+    const test = ref("11111");
     const state = reactive({
       todoList: ["123123123", "sadasdasdsa"],
       count: 0,
@@ -23,10 +27,14 @@ export default {
     function increment() {
       state.todoList = [...state.todoList, "3213123"];
     }
-
+    function console() {
+      window.console.log(test.value);
+    }
     return {
+      test,
       state,
       increment,
+      console,
     };
   },
 };
