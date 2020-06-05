bbe81018a80870407be3fb0e062e9c371f9998e2
chenc <chenc@fengyuntec.com>
2020-06-05 11:17:40 +0800

    ✨ 添加vue3demo

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import router from "./router";
 import store from "./store";
 import "./plugins/element.js";
 import "./components";
+import VueCompositionApi from "@vue/composition-api";
+
+Vue.use(VueCompositionApi);
+
 Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/tab1.vue
+++ src/views/tab1.vue
@@ -1,13 +1,29 @@
 <template>
-    <div>tab1</div>
+  <button @click="increment">
+    Count is: {{ state.count }}, double is: {{ state.double }}
+  </button>
 </template>
 
 <script>
-    export default {
-        name: "tab1"
+import { reactive, computed } from "@vue/composition-api";
+export default {
+  name: "tab1",
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+    });
+
+    function increment() {
+      state.count++;
     }
+
+    return {
+      state,
+      increment,
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
