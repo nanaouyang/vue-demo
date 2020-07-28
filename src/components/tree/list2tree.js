ecb6fbffaf0a54cf8b0e6a47750cff5be89afdf9
chenc <chenc@fengyuntec.com>
2020-07-28 11:16:23 +0800

    test

--- src/components/tree/list2tree.js
+++ src/components/tree/list2tree.js
@@ -6,8 +6,8 @@ export function list2tree(
     const obj = list.find((item) => item[id] === curr[pid]); // 根据当前项找出父节点
     if (obj) {
       // 存在父节点
-      // eslint-disable-next-line no-prototype-builtins
-      !obj.hasOwnProperty([children]) && (obj[children] = []); // 父节点无[children]属性，初始化[children]
+      !Object.prototype.hasOwnProperty.call(obj, [children]) &&
+        (obj[children] = []); // 父节点无[children]属性，初始化[children]
       obj[children].push(curr); // 把当前节点推进父节点[children]
     } else {
       prev.push(curr);
